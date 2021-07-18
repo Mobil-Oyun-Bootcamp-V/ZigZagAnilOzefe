f3c2f896d96774f8b25fadb6b641a649f1874c7d
Anıl Özefe <84273773+anilozefe@users.noreply.github.com>
2021-07-18 22:52:07 +0300

    .

---
+++ Assets/script/pausa.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class pausa : MonoBehaviour {
+
+	// Use this for initialization
+
+	bool pausado=false;
+
+	public void Pause() {
+		if (!pausado) {
+
+			Time.timeScale = 0;
+			pausado = true;
+		} else {
+
+			Time.timeScale = 1;
+			pausado=false;
+		}
+	}
+
+}
